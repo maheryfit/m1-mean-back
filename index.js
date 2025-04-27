63c77f0194f2b7ebd59c0bae02a423fbabdb4739
Eriq606 <erohweltall@gmail.com>
2025-04-27 17:21:11 +0300

    [FIX] import CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import clientRouter from "./routers/ClientRouter.js";
 import {ToolingMiddleware} from "./middlewares/ToolingMiddleware.js";
