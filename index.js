b18040d1d41595579ebff3a18446dadd06fb385b
Eriq606 <erohweltall@gmail.com>
2025-04-27 17:03:08 +0300

    [FIX] CORS

--- index.js
+++ index.js
@@ -27,13 +27,7 @@ const connection=new MongoClient(url);
 await connection.connect();
 
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin: ["*"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-    }
-))
+app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use([
