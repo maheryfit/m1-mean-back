414a14ff6647c421d8242c404f46769a5de04f41
Eriq606 <erohweltall@gmail.com>
2025-04-28 02:22:41 +0300

    [FIX] CORS allow credentials

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ await connection.connect();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
--- stop.sh
+++ stop.sh
@@ -1,6 +1,4 @@
 docker stop redis-cache
 docker stop app-server-cont
 docker stop mongo-cont
-
-docker network remove mern-app
 docker volume remove mongo_db_refacto
