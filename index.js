358148baa2ddc79ff1c7fafc18040b0de4557789
Eriq606 <erohweltall@gmail.com>
2025-04-28 03:32:09 +0300

    [FIX] CORS single origin

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const connection=new MongoClient(url);
 await connection.connect();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200, http://46.101.29.136");
+    res.header("Access-Control-Allow-Origin", "http://46.101.29.136");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
