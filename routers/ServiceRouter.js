b7c3384729d8da4f1f35c0a49b0361ad3597b632
mahery <fitahiana.mahery@futurmap.com>
2025-05-12 16:55:25 +0300

    [REFACTORISATION] station: liste

--- routers/ServiceRouter.js
+++ routers/ServiceRouter.js
@@ -20,4 +20,21 @@ serviceRouter.get("/liste-service/:page/:limit", AuthMiddleware.checkConnecte, a
    }
 });
 
+serviceRouter.get("/count", AuthMiddleware.checkConnecte, async (req,res)=>{
+    try {
+        const count = await Service.countServices(req.myConnection, null, req.config);
+        res.status(200).json(count);
+    } catch (error) {
+        res.status(500).send({message:error.message});
+    }
+})
+
+serviceRouter.get("/:page/:limit", AuthMiddleware.checkConnecte, async (req,res)=>{
+    try {
+        const services = await Service.getServices(req.myConnection, null, req.config, req.params.page, req.params.limit);
+        res.status(200).json(services);
+    } catch (error) {
+        res.status(500).send({message:error.message});
+    }
+})
 export default serviceRouter;
